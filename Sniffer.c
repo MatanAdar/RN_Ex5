d76840528fd1955174e7cb8dcaf9bf78a596342a
MatanAdar <matan.adar@msmail.ariel.ac.il>
2023-01-17 23:12:32 +0200

    finish sniffer

--- Sniffer.c
+++ Sniffer.c
@@ -6,7 +6,8 @@
 #include <netinet/tcp.h>
 #include <time.h>
 #include <unistd.h>
-#define SIZE_ETHERNET 14
+
+/* Ethernet header */
 struct ethheader
 {
   u_char ether_dhost[ETHER_ADDR_LEN]; /* destination host address */
@@ -14,6 +15,7 @@ struct ethheader
   u_short ether_type;                 /* IP? ARP? RARP? etc */
 };
 
+/* IP Header */
 struct ipheader
 {
   unsigned char iph_ihl : 4,       // IP header length
@@ -30,62 +32,57 @@ struct ipheader
   struct in_addr iph_destip;       // Destination IP address
 };
 
+//Application Header
 struct app_header
 {
-    uint32_t timestamp;
-    uint16_t length;
+  uint32_t timestamp;              
+  uint16_t length;                //total length of the packet
 
-    union
-    {
-        uint16_t flags;
-        uint16_t _:3, c_flag:1, s_flag:1, t_flag:1, status:10;
-    };
-    
-    uint16_t cache;
-    uint16_t __;
+  union                          // all the flags and the status is in here and we taking specific bytes to each flag
+  {
+    uint16_t flags;
+    uint16_t _ : 3, c_flag : 1, s_flag : 1, t_flag : 1, status : 10;   //The status code of the response
+  };
+
+  uint16_t cache;                //'Max-Age' value for the cache.
+  uint16_t __;
 };
-
-
 
 void got_packet(u_char *args, const struct pcap_pkthdr *header,
                 const u_char *packet)
 {
+  //open file by our id names
+  FILE *file;
+  file = fopen("213861529_209321553.txt", "a+");
+  if (file == NULL)
+  {
+    printf("unable to create file");
+  }
 
-FILE *file;
-file=fopen("213861529_209321553.txt","a+");
-if(file==NULL)
-{
-  printf("unable to create file");
-}
-
-  
-  struct tcphdr *tcphdr;
-  
   struct ethheader *eth = (struct ethheader *)packet;
 
-  struct app_header *apphdr;
+  // Find where the IP header starts, and typecast it to the IP Header structure
+  struct ipheader *ip = (struct ipheader *)(packet + sizeof(struct ethheader));
+  unsigned short iphdrlen = (ip->iph_ihl) * 4;
 
-  
-  time_t rawtime=header->ts.tv_sec;
+  // Find where the tcp header starts, and typecast it to the tcp header structure
+  struct tcphdr *tcphdr= (struct tcphdr *)(packet + iphdrlen + sizeof(struct ethheader));
+  unsigned int data_offset = (tcphdr->th_off) * 4;
+
+  // Find where the aaplication header starts, and typecast it to the application Header structure
+  struct app_header *apphdr= (struct app_header *)(packet + sizeof(struct ethheader) + iphdrlen + data_offset);
+
+
+  // print the timestamp as a human readable date and time
+  time_t rawtime = header->ts.tv_sec;
   struct tm date;
   char buf[80];
+  date = *localtime(&rawtime);
+  strftime(buf, sizeof(buf), "%a %d-%m-%Y %H:%M:%S", &date);
 
-  date=*localtime(&rawtime);
-  strftime(buf,sizeof(buf),"%a %d-%m-%Y %H:%M:%S",&date);
-  
-  
+
   if (ntohs(eth->ether_type) == 0x0800)
   { // 0x0800 is IP type
-
-    struct ipheader *ip = (struct ipheader *)(packet + sizeof(struct ethheader));
-    unsigned short iphdrlen = (ip->iph_ihl)*4;
-    tcphdr= (struct tcphdr*)(packet + iphdrlen +sizeof(struct ethheader));
-    unsigned int data_offset = (tcphdr->th_off)* 4;
-    apphdr= (struct app_header*)(packet+sizeof(struct ethheader)+iphdrlen+tcphdr->doff*4);
-
-    // struct tcphdr* tcp_header = (struct tcphdr*) (packet + sizeof(struct ethhdr) + sizeof(struct ipheader));
-    // unsigned int timestamp = tcp_header->th_timestamp;
-    // time_t epoch_time = (time_t) timestamp;
 
     /* determine protocol */
     switch (ip->iph_protocol)
@@ -93,55 +90,54 @@ if(file==NULL)
     case IPPROTO_TCP:
 
       printf("   Protocol: TCP\n");
-      
-    
-    
-      fprintf(file,"                        TCP header\n");
-      fprintf(file,"**********************************************************************\n");
-      fprintf(file,"           source_ip: %s\n", inet_ntoa(ip->iph_sourceip));
-      fprintf(file,"           dest_ip: %s\n", inet_ntoa(ip->iph_destip));
-      fprintf(file,"           source_port: %hu\n",ntohs(tcphdr->source));
-      fprintf(file,"           dst_port: %hu\n",ntohs(tcphdr->dest));
-      fprintf(file,"           timestap: %u (%s)\n",ntohl(apphdr->timestamp),buf);
-      fprintf(file,"           total_length:%u \n",ntohs(apphdr->length));
-      fprintf(file,"           cache_flag: %u \n",(ntohs(apphdr->flags)>>12) & 1);
-      fprintf(file,"           step_flag: %u \n",(ntohs(apphdr->flags)>>11) & 1);
-      fprintf(file,"           type_flag: %u \n",(htons(apphdr->flags)>>10) & 1);
-      fprintf(file,"           status_code: %u \n",htons(apphdr->flags)& 0x3ff);
-      fprintf(file,"           cache_control:%u \n",ntohs(apphdr->cache));
-      fprintf(file,"           data: \n");
-      
-      //exctract data from the application header and print it to the file
-      u_char * data = (u_char * )(packet + sizeof(struct ethheader)+iphdrlen+ data_offset+ sizeof(apphdr));
+
+      fprintf(file, "                        TCP header\n");
+      fprintf(file, "** ********************************************************************\n");
+      fprintf(file, "           source_ip: %s\n", inet_ntoa(ip->iph_sourceip));
+      fprintf(file, "           dest_ip: %s\n", inet_ntoa(ip->iph_destip));
+      fprintf(file, "           source_port: %hu\n", ntohs(tcphdr->source));
+      fprintf(file, "           dst_port: %hu\n", ntohs(tcphdr->dest));
+      fprintf(file, "           timestap: %u (%s)\n", ntohl(apphdr->timestamp), buf);
+      fprintf(file, "           total_length:%u \n", ntohs(apphdr->length));
+      fprintf(file, "           cache_flag: %u \n", (ntohs(apphdr->flags) >> 12) & 1);
+      fprintf(file, "           step_flag: %u \n", (ntohs(apphdr->flags) >> 11) & 1);
+      fprintf(file, "           type_flag: %u \n", (htons(apphdr->flags) >> 10) & 1);
+      fprintf(file, "           status_code: %u \n", htons(apphdr->flags) & 0x3ff);
+      fprintf(file, "           cache_control:%u \n", ntohs(apphdr->cache));
+      fprintf(file, "           data: \n");
+
+      // exctract data from the application header and print it to the file
+      u_char *data = (u_char *)(packet + sizeof(struct ethheader) + iphdrlen + data_offset + sizeof(apphdr));
       unsigned int data_size = ntohs(ip->iph_len) - (iphdrlen + data_offset);
 
-      if(data_size>0)
+      if (data_size > 0)
       {
-        for(int i=0;i<data_size;i++)
+        for (int i = 0; i < data_size; i++)
         {
-          if(!(i & 15))
+          if (!(i & 15))
           {
-            fprintf(file,"\n %04X: ",i);
+            fprintf(file, "\n %04X: ", i);
           }
-          fprintf(file,"%02X: ",((unsigned char*)data)[i]);
-          
+          fprintf(file, "%02X: ", ((unsigned char *)data)[i]);
         }
       }
       else
       {
-        fprintf(file,"                     dont receive any data!");
+        fprintf(file, "                     dont receive any data!");
       }
-      fprintf(file,"\n");
-      fprintf(file,"**********************************************************************\n");
+      fprintf(file, "\n");
+      fprintf(file, "**********************************************************************\n");
       fclose(file);
-    
       return;
+
     case IPPROTO_UDP:
       printf("   Protocol: UDP\n");
       return;
+
     case IPPROTO_ICMP:
       printf("   Protocol: ICMP\n");
       return;
+
     default:
       printf("   Protocol: others\n");
       return;
@@ -149,14 +145,11 @@ if(file==NULL)
   }
 }
 
-
 // void got_packet(u_char *args, const struct pcap_pkthdr *header,
 //                 const u_char *packet)
 // {
 //   printf("Got a packet\n");
 // }
-
-
 
 int main()
 {
@@ -165,7 +158,7 @@ int main()
   struct bpf_program fp;
   char filter_exp[] = "tcp";
   bpf_u_int32 net;
-  
+
   // Step 1: Open live pcap session on NIC with name eth3
   handle = pcap_open_live("lo", BUFSIZ, 1, 1000, errbuf);
 
@@ -178,9 +171,5 @@ int main()
 
   pcap_close(handle); // Close the handle
 
-  
-
   return 0;
-
-
 }
